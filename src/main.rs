c2443d2f8fc35ffb1299a6761e751fdb9101d40b
Xavier Perez <xperez@fastly.com>
2018-11-09 23:57:46 -0800

    Woweee almost works

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,16 @@ extern crate rand;
 extern crate rustbox;
 extern crate sdl2;
 
-use std::io::{Write, stdout, stdin};
+// use std::io::{Write, stdout, stdin};
 use std::{time, thread};
 use rand::Rng;
-use std::error::Error;
-use std::default::Default;
+// use std::error::Error;
+// use std::default::Default;
 
-use rustbox::{RustBox};
-use rustbox::Key;
+// use rustbox::{RustBox};
+// use rustbox::Key;
 
-use sdl2::pixels::{Color,PixelFormatEnum};
+use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
@@ -33,11 +33,12 @@ struct Cpu {
     /// Graphics. 2048 pixels
     pub gfx: Vec<u8>,
     /// Delay timer
-    delay: u16,
+    delay: u8,
     /// Sound Timer
-    sound: u16,
+    sound: u8,
     /// Whether to draw graphics
     should_draw: bool,
+    key_pressed: Option<u8>,
 }
 
 impl Cpu {
@@ -56,38 +57,16 @@ impl Cpu {
             delay: 0,
             sound: 0,
             should_draw: false,
+            key_pressed: None,
         }
     }
 
     fn step(&mut self) {
-        // let prog = vec![
-        //     0x6100, // LD V1, 0 ; x
-        //     0x620A, // LD V2, 0 ; y
-        //     0x6307, // LD V3, 3 ; number to draw
-        //     0x6400, // LD V4, 0 ; character counter
-        //     0x650A, // LD V5, 0 ; character to draw
-        //     0x9340, // SKIP if X != Y
-        //     0x1220, // JUMP TO EXIT $220
-        //     0xF529, // SPR V5
-        //     0xD125, // DRAW V1 V2 5 ; draw x=v1, y=v2, height=5
-        //     0x7105, // ADD V1, 5
-        //     0x7401, // ADD V4, 1
-        //     0x7501, // ADD V5, 1
-        //     0x120A, // JMP to loop $210
-        //     0x00E0  // CLS
-        // ];
-
-        // for (i, item) in prog.iter().enumerate() {
-        //     self.store_16(0x200 + (i as u16) * 2, (*item) as u16);
-        // }
-
-        // self.store_16(0x200, 0x600F);
-        // self.store_16(0x202, 0xF029);
-        // self.store_16(0x204, 0xD005);
         self.should_draw = false;
 
         let instruction = self.load_16(self.pc);
-        //println!("{:04x}: {:04x} {:?} I:{:04x} S:{:02x}", self.pc, instruction, self.regs, self.i, self.sp);
+        // println!("{:04x}: {:04x} {:?} I:{:04x} S:{:02x} {:?}", self.pc, instruction, self.regs, self.i, self.sp, self.stack);
+        self.pc += 2;
 
         let a = instruction >> 12;
         let x = ((instruction >> 8) & 0xF) as usize;
@@ -99,28 +78,25 @@ impl Cpu {
         match a {
             0x0 => {
                 match n {
-                    0 => {}, // TODO: Clear screen
+                    0 => self.gfx = vec![0; 64 * 32], // FIXME: zero out instead of reallocating
                     _ => {
-                        // return from subroutine
                         self.sp -= 1;
                         let address = self.stack[self.sp];
-                        self.pc = address - 2; // oh god why
+                        self.pc = address;
                     }
                 }
             }
-            // Jump
             0x1 => self.pc = nnn,
-            // Call subroutine
             0x2 => {
                 self.stack[self.sp] = self.pc;
                 self.sp += 1;
-                self.pc = nnn - 2; // Hackish oh god
+                self.pc = nnn;
             },
-            0x3 => if self.regs[x] == 0 { self.pc += 2; },
-            0x4 => if self.regs[x] != 0 { self.pc += 2; },
+            0x3 => if self.regs[x] == nn { self.pc += 2; },
+            0x4 => if self.regs[x] != nn { self.pc += 2; },
             0x5 => if self.regs[x] == self.regs[y] { self.pc += 2; },
             0x6 => self.regs[x] = nn,
-            0x7 => self.regs[x] += nn,
+            0x7 => self.regs[x] = self.regs[x].wrapping_add(nn),
             0x8 => {
                 match n {
                     0x0 => self.regs[x] = self.regs[y],
@@ -136,7 +112,15 @@ impl Cpu {
                         }
                         self.regs[x] = res as u8;
                     },
-                    0x5 => unimplemented!("VX-VY"),
+                    0x5 => {
+                        let res = (self.regs[x] as u32).wrapping_sub(self.regs[y] as u32);
+                        if res & 0x100 == 0 {
+                            self.regs[0xF] = 0;
+                        } else {
+                            self.regs[0xF] = 1;
+                        }
+                        self.regs[x] = res as u8;
+                    },
                     0x6 => {
                         self.regs[0xF] = self.regs[x] & 1;
                         self.regs[x] >>= 1;
@@ -175,10 +159,33 @@ impl Cpu {
                     }
                 }
             },
-            0xE => unimplemented!("0xE Keys"),
+            0xE => {
+                match nn {
+                    0x9E => {
+                        if let Some(key) = self.key_pressed {
+                            if key == self.regs[x] {
+                                self.pc += 2;
+                            }
+                        }
+                    },
+                    0xA1 => {
+                        if let Some(key) = self.key_pressed {
+                            if key != self.regs[x] {
+                                self.pc += 2;
+                            }
+                        } else {
+                            self.pc += 2;
+                        }
+                    }
+                    _ => unimplemented!("keys stuff"),
+                }
+            }
             0xF => {
                 match nn {
-                    // TODO: 07, 0A, 15, 18
+                    // TODO: 0A
+                    0x07 => self.regs[x] = self.delay,
+                    0x15 => self.delay = self.regs[x],
+                    0x18 => self.sound = self.regs[x],
                     0x1E => self.i += self.regs[x] as u16,
                     0x29 => self.i = self.regs[x] as u16 * 5,
                     0x33 => {
@@ -207,9 +214,7 @@ impl Cpu {
         };
 
         if self.delay > 0 { self.delay -= 1 };
-        if self.sound > 0 { self.sound -= 1 }; // TODO: Emit beep
-
-        self.pc += 2;
+        if self.sound > 0 { print!("BEEP"); self.sound -= 1 }; // TODO: Emit beep
     }
 
     fn load_16(&self, address: u16) -> u16 {
@@ -218,10 +223,10 @@ impl Cpu {
         (hi << 8) + lo
     } 
 
-    fn store_16(&mut self, address: u16, value: u16) {
-        self.mem[address as usize] = (value >> 8) as u8;
-        self.mem[address as usize + 1] = (value & 0xFF) as u8;
-    }
+    // fn store_16(&mut self, address: u16, value: u16) {
+    //     self.mem[address as usize] = (value >> 8) as u8;
+    //     self.mem[address as usize + 1] = (value & 0xFF) as u8;
+    // }
 
     fn fontset() -> Vec<u8> {
         vec![
@@ -275,6 +280,21 @@ fn main() {
     let mut event_pump = sdl_context.event_pump().unwrap();
 
     'running: loop {
+        while let Some(event) = event_pump.poll_event() {
+            match event {
+                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                Event::KeyUp { .. } => {
+                    //cpu.key_pressed = None;
+                },
+                Event::KeyDown { keycode: Some(keycode), .. } => {
+                    cpu.key_pressed = keycode_to_u8(keycode);
+                } 
+                _ => ()
+            }
+        }
+
         cpu.step();
 
         if cpu.should_draw {
@@ -287,21 +307,14 @@ fn main() {
                     match cpu.gfx[(column + row * 64) as usize] {
                         1 => canvas.fill_rect(Rect::new(column * 8, row * 8, 8, 8)),
                         _ => Ok(()),
-                    };
+                    }.unwrap();
                 };
             }
 
             canvas.present();
         }
 
-        while let Some(event) = event_pump.poll_event() {
-            match event {
-                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
-                },
-                _ => ()
-            }
-        }
+        thread::sleep(time::Duration::from_secs(1) / 240);
 
         // rustbox.present();
         // match rustbox.poll_event(false) {
@@ -320,3 +333,41 @@ fn main() {
     // write!(screen, "{}", termion::cursor::Show).unwrap();
     // print!("{}", termion::cursor::Show);
 }
+
+fn keycode_to_u8(keycode: Keycode) -> Option<u8> {
+    match keycode {
+        Keycode::Num1 => Some(1),
+        Keycode::Num2 => Some(2),
+        Keycode::Num4 => Some(4),
+        Keycode::Num6 => Some(6),
+        Keycode::Num8 => Some(8),
+        Keycode::C => Some(0xC),
+        Keycode::D => Some(0xD),
+        _ => None
+    }
+}
+
+// let prog = vec![
+//     0x6100, // LD V1, 0 ; x
+//     0x620A, // LD V2, 0 ; y
+//     0x6307, // LD V3, 3 ; number to draw
+//     0x6400, // LD V4, 0 ; character counter
+//     0x650A, // LD V5, 0 ; character to draw
+//     0x9340, // SKIP if X != Y
+//     0x1220, // JUMP TO EXIT $220
+//     0xF529, // SPR V5
+//     0xD125, // DRAW V1 V2 5 ; draw x=v1, y=v2, height=5
+//     0x7105, // ADD V1, 5
+//     0x7401, // ADD V4, 1
+//     0x7501, // ADD V5, 1
+//     0x120A, // JMP to loop $210
+//     0x00E0  // CLS
+// ];
+
+// for (i, item) in prog.iter().enumerate() {
+//     self.store_16(0x200 + (i as u16) * 2, (*item) as u16);
+// }
+
+// self.store_16(0x200, 0x600F);
+// self.store_16(0x202, 0xF029);
+// self.store_16(0x204, 0xD005);
